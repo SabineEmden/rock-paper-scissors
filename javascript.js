8e75b7fa2495edb7e7f38b0ef005e96c9c17410b
Sabine Emden <EmdenSabine@gmail.com>
2024-03-21 11:57:27 -0400

    Add function getComputerChoice
    This function is used in the game to make the computer's play.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+// Create a function getComputerChoice that randomly returns either 'Rock', 'Paper' or 'Scissors'.
+function getComputerChoice() {
+  switch (Math.floor(Math.random() * 3)) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+}
